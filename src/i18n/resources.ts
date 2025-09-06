674e88ce680543fe60102671f6678a3f08ece3af
edbns <souhil@protonmail.ch>
2025-09-07 07:52:55 +0800

    Delete src directory

--- src/i18n/resources.ts
+++
@@ -1,9 +0,0 @@
-import en from './locales/en.json';
-import fr from './locales/fr.json';
-import ar from './locales/ar.json';
-
-export const resources = {
-  en: { translation: en },
-  fr: { translation: fr },
-  ar: { translation: ar },
-};
